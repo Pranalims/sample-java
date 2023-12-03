09ce30ef48636e1ab1566fe777383ca0e9811ec0
pranali Mhatre <mhatrepranali14@gmail.com>
2023-12-03 19:19:22 +0530

    testone created

---
+++ src/mpack/testone.java
@@ -0,0 +1,21 @@
+package mpack;
+
+public class testone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int custno=101;
+String custname="abc";
+boolean status=true;
+double salary=10000.05;
+methodOne();
+
+System.out.println(status);
+	}
+public static void methodOne() {
+	System.out.println("calling methodOne");
+	
+}
+	
+
+}
