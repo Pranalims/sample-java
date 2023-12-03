fe3775b875a7b91223de1fca129f901b579fa073
pranali Mhatre <mhatrepranali14@gmail.com>
2023-12-03 19:35:12 +0530

    updated code

--- src/mpack/testone.java
+++ src/mpack/testone.java
@@ -9,8 +9,10 @@ String custname="abc";
 boolean status=true;
 double salary=10000.05;
 methodOne();
+int a=5,b=3;
 
 System.out.println(status);
+System.out.println(a+b);
 	}
 public static void methodOne() {
 	System.out.println("calling methodOne");
